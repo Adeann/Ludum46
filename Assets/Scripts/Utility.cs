2935cf716e80a6037999614374329704e5f98765
Jonathan Foster <jfoste57@gmail.com>
2020-04-19 13:51:01 -0400

    implementing pause

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    public static GameObject gameObject;
+
+    public static class GameState
+    {
+        public static bool isPaused;
+        public static float gameTime = 0f;
+
+        public static void Pause()
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+        }
+        public static void UnPause()
+        {
+            isPaused = false;
+            Time.timeScale = 1f;
+        }
+
+        public static void Toggle()
+        {
+            if (isPaused)
+                UnPause();
+            else
+                Pause();
+        }
+
+    }
+
+
+}
